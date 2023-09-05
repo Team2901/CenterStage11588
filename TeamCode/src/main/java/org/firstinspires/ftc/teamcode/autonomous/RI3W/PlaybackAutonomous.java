102e60cdb72ff7d160fbaf1a681f7cefca5d138a
NickJerden <111648197+NickJerden@users.noreply.github.com>
2023-09-05 15:13:19 -0400

    Record and playback teleop (#4)
    * Created Record and Playback teleop branch
    
    * Added more controller inputs to the PlaybackAutonomous and RecordingTeleop
    
    * Added reading exact ms to the playback auto
    
    * Changed the playback to wait for the next ms instruction and increased precsions by a lot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RI3W/PlaybackAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RI3W/PlaybackAutonomous.java
@@ -11,10 +11,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-//test
 @Autonomous(name="Playback Auto", group="11588")
 public class PlaybackAutonomous extends LinearOpMode {
     RI3WHardware robot = new RI3WHardware();
+    Boolean checkedTimer = false;
     ElapsedTime timer = new ElapsedTime();
     FileInputStream fileInputStream = new FileInputStream(Environment.getExternalStorageDirectory().getAbsolutePath() + "/test.log");
     DataInputStream readFile = new DataInputStream(fileInputStream);
@@ -39,27 +39,65 @@ public class PlaybackAutonomous extends LinearOpMode {
     }
 
     private void autoLoop() throws IOException {
-        int currentMiliseconds = (int) Math.floor(timer.milliseconds());
-        double ry = 0;
-        double rx = 0;
-        double ly = 0;
-        double lx = 0;
-        if (currentMiliseconds > lastKnownMilisecond) {
-            lastKnownMilisecond = currentMiliseconds;
-            ry = readFile.readFloat();
-            rx = readFile.readFloat();
-            ly = readFile.readFloat();
-            lx = readFile.readFloat();
-            turningPower = .75 * rx;
-            double y = .75 * ly;
-            double x = .75 * lx;
-            double rightX = turningPower;
+        double currentMiliseconds = timer.milliseconds();
+        double fileMiliseconds = 0;
+        double rightStickY = 0;
+        double rightStickX = 0;
+        double leftStickY = 0;
+        double leftStickX = 0;
+        boolean aButton;
+        boolean bButton;
+        boolean xButton;
+        boolean yButton;
+        boolean dpadUp;
+        boolean dpadDown;
+        boolean dpadLeft;
+        boolean dpadRight;
+        boolean rightBumper;
+        boolean leftBumper;
+        float rightTrigger = 0;
+        float leftTrigger = 0;
 
-            robot.frontLeft.setPower(y + x + rightX);
-            robot.frontRight.setPower(y - x - rightX);
-            robot.backLeft.setPower(y - x + rightX);
-            robot.backRight.setPower(y + x - rightX);
+        if (checkedTimer == false) {
+           fileMiliseconds = readFile.readDouble();
+           checkedTimer = true;
         }
+        /*I wrote the above statement to keep it from reading the next byte before the time for those
+        instructions has been executed. I don't want it to keep reading the next double before it
+        executes the instructions that come after that double, because in sequence the next instruction
+        is not a double, but a float(Because it should be the joystick value at that ms)
+         */
+        while (currentMiliseconds < fileMiliseconds) {
+            currentMiliseconds = timer.milliseconds();
+        }
+            //fileMiliseconds means the double value read for the ms that all the joystick and button-
+            //values were recorded at
+            rightStickY = readFile.readFloat();
+            rightStickX = readFile.readFloat();
+            leftStickY = readFile.readFloat();
+            leftStickX = readFile.readFloat();
+            aButton = readFile.readBoolean();
+            bButton = readFile.readBoolean();
+            xButton = readFile.readBoolean();
+            yButton = readFile.readBoolean();
+            dpadUp = readFile.readBoolean();
+            dpadDown = readFile.readBoolean();
+            dpadLeft = readFile.readBoolean();
+            dpadRight = readFile.readBoolean();
+            rightBumper = readFile.readBoolean();
+            leftBumper = readFile.readBoolean();
+            rightTrigger = readFile.readFloat();
+            leftTrigger = readFile.readFloat();
+            turningPower = .75 * rightStickX;
+            double y = .75 * leftStickY;
+            double x = .75 * leftStickX;
+            double rx = turningPower;
+
+            robot.frontLeft.setPower(y + x + rx);
+            robot.frontRight.setPower(y - x - rx);
+            robot.backLeft.setPower(y - x + rx);
+            robot.backRight.setPower(y + x - rx);
+            checkedTimer = false;
     }
 
 }
