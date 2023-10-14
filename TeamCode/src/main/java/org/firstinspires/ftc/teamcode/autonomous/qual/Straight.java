3e0785255482ddcab79a3c9904fd7e4465cfaccc
mcgeeo27445 <mcgeeo27445@student.cghsnc.org>
2023-10-14 11:51:51 -0400

    init robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/qual/Straight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/qual/Straight.java
@@ -5,7 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name="Straight", group="11588")
 public class Straight extends AbstractAutonomous {
 
+
+
     public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap,telemetry);
+        waitForStart();
         //Go straight and turn
         moveXY(24, 0);
         //turn right
