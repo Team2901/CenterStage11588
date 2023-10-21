395f6ff8e2da09a882743e9b11256da7999a2bd3
NickJerden <jerdenn25556@student.cghsnc.org>
2023-10-21 11:18:54 -0400

    Commented out AprilTagTestingAutonomous.java, it wasn't suppose to be in this branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/qual/QualTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/qual/QualTeleop.java
@@ -1,8 +1,7 @@
-package org.firstinspires.ftc.teamcode.teleop.Qual;
+package org.firstinspires.ftc.teamcode.teleop.qual;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.hardware.Qual.QualHardware;
