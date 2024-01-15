4983a64076b4a7df4693251cce36081a5a9629ff
NickJerden <jerdenn25556@student.cghsnc.org>
2024-01-15 13:29:09 -0500

    Proportional controller finished pt 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/qual/QualTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/qual/QualTeleop.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.teleop.qual;
+package org.firstinspires.ftc.teamcode.teleop.Qual;
 
 
 
@@ -6,20 +6,20 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.hardware.RI3W.RI3WHardware;
 import org.firstinspires.ftc.teamcode.hardware.qual.QualHardware;
 import org.firstinspires.ftc.teamcode.hardware.controller.ImprovedGamepad;
-import org.firstinspires.ftc.teamcode.teleop.RI3W.RI3WTeleop;
 
 
-@TeleOp(name="Qual Teleop", group="11588 Quals")
+@TeleOp(name="Qual Bulldozer Teleop", group="11588 Quals")
 public class QualTeleop extends OpMode {
+    public static final double DROPPER_OPEN_POSITION = .5;
+    public static final int ARM_SERVO_POSITION = 1;
     public QualHardware robot = new QualHardware();
     public ImprovedGamepad gamepad;
     double turningPower = 0;
     ElapsedTime PIDTimer = new ElapsedTime();
-    public enum Height{INTAKE, MAX}
-    Height currentLiftHeight = Height.INTAKE;
+    public enum Height{GROUND, FIRST, SECOND, THIRD}
+    Height currentLiftHeight = Height.GROUND;
     int liftTarget = 80;
     Height lastLiftHeight = currentLiftHeight;
 
@@ -41,34 +41,33 @@ public class QualTeleop extends OpMode {
         } else {
             turningPower = .75 * gamepad.right_stick_x.getValue();
         }
-/*
-        if(gamepad.left_bumper.isInitialPress()) {
-            if (robot.draggerPosition == QualHardware.DraggerPosition.UP) {
-                robot.draggerPosition = QualHardware.DraggerPosition.DOWN;
-                robot.dragger.setPosition(QualHardware.DRAGGER_DOWN_POSITION);
-            } else if (robot.draggerPosition == QualHardware.DraggerPosition.DOWN) {
-                robot.draggerPosition = QualHardware.DraggerPosition.UP;
 
-                robot.dragger.setPosition(QualHardware.DRAGGER_UP_POSITION);
-            }
-        }
-*/
-
-        if (gamepad.dpad_down.isPressed()) {
-            liftTarget = QualHardware.INTAKE_ENCODER_VALUE;
-            currentLiftHeight = Height.INTAKE;
-        } else if (gamepad.dpad_up.isInitialPress()) {
-            liftTarget = QualHardware.MAX_HEIGHT_ENCODER_VALUE;
-            currentLiftHeight = Height.MAX;
+        if (gamepad.y.isPressed()) {
+            robot.intake.setPower(.75);
+        } else if (gamepad.x.isPressed()) {
+            robot.intake.setPower(-.75);
         }
 
-        if(gamepad.left_bumper.isInitialPress()) {
-            liftTarget -= 10;
-        } else if(gamepad.right_bumper.isInitialPress()) {
-            liftTarget += 10;
+        if (gamepad.a.isInitialPress()) {
+            robot.purplePixelDropper.setPosition(DROPPER_OPEN_POSITION);
         }
 
-        robot.lift.setPower(liftPower(liftTarget));
+//        if (gamepad.b.isPressed() && robot.armRight.getPosition() == 0) {
+//            robot.armRight.setPosition(ARM_SERVO_POSITION);
+//            robot.armLeft.setPosition(ARM_SERVO_POSITION);
+//        } else {
+//            robot.armRight.setPosition(0);
+//            robot.armLeft.setPosition(0);
+//        }
+
+
+        if(gamepad.left_bumper.isPressed() && robot.goalPosition > 5) {
+            robot.goalPosition -= 5;
+        } else if(gamepad.right_bumper.isPressed() && robot.goalPosition < robot.MAX_LIFT_HEIGHT) {
+            robot.goalPosition += 5;
+        }
+
+        robot.PIDLoop();
 
         double y = 1 * gamepad.left_stick_y.getValue();
         double x = 1 * gamepad.left_stick_x.getValue();
@@ -84,37 +83,39 @@ public class QualTeleop extends OpMode {
         telemetry.addData("Left", gamepad.left_stick_y.getValue());
         telemetry.addData("Lift Speed", robot.liftSpeed);
         telemetry.addData("Lift Position", robot.lift.getCurrentPosition());
-        telemetry.addData("Lift Target", liftTarget);
+        telemetry.addData("Lift Target", robot.goalPosition);
+//        telemetry.addData("Arm Left servo", robot.armLeft.getPosition());
+//        telemetry.addData("Arm Right servo", robot.armRight.getPosition());
         telemetry.update();
 
     }
 
-    public double liftPower(int target){
-        robot.error = target - robot.lift.getCurrentPosition();
-        robot.dLift = (robot.error - robot.pLift) / PIDTimer.seconds();
-        robot.iLift = robot.iLift + (robot.error * PIDTimer.seconds());
-        robot.pLift = robot.error;
-        robot.total = ((robot.pLift * QualHardware.KP) + (robot.iLift * QualHardware.KI) + (robot.dLift * QualHardware.KD))/100;
-        PIDTimer.reset();
-
-
-        if(currentLiftHeight != lastLiftHeight){
-            robot.iLift = 0;
-        }
-        if(robot.iLift > robot.iLiftMax){
-            robot.iLift = robot.iLiftMax;
-        }else if(robot.iLift < -robot.iLiftMax){
-            robot.iLift = -robot.iLiftMax;
-        }
-        if(robot.total > .5){
-            robot.total = .5;
-        }
-
-        robot.total = ((robot.pLift * QualHardware.KP) + (robot.iLift * QualHardware.KI) + (robot.dLift * QualHardware.KD))/100 +QualHardware.KG;
-
-        lastLiftHeight = currentLiftHeight;
-
-        return robot.total;
-    }
+//    public double liftPower(int target){
+//        robot.error = target - robot.lift.getCurrentPosition();
+//        robot.dLift = (robot.error - robot.pLift) / PIDTimer.seconds();
+//        robot.iLift = robot.iLift + (robot.error * PIDTimer.seconds());
+//        robot.pLift = robot.error;
+//        robot.total = ((robot.pLift * QualHardware.KP) + (robot.iLift * QualHardware.KI) + (robot.dLift * QualHardware.KD))/100;
+//        PIDTimer.reset();
+//
+//
+//        if(currentLiftHeight != lastLiftHeight){
+//            robot.iLift = 0;
+//        }
+//        if(robot.iLift > robot.iLiftMax){
+//            robot.iLift = robot.iLiftMax;
+//        }else if(robot.iLift < -robot.iLiftMax){
+//            robot.iLift = -robot.iLiftMax;
+//        }
+//        if(robot.total > .5){
+//            robot.total = .5;
+//        }
+//
+//        robot.total = ((robot.pLift * QualHardware.KP) + (robot.iLift * QualHardware.KI) + (robot.dLift * QualHardware.KD))/100 +QualHardware.KG;
+//
+//        lastLiftHeight = currentLiftHeight;
+//
+//        return robot.total;
+//    }
 
 }
